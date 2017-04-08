23a3fe50aa47f7d0475060636132ac35150304c8
Greg Scott <gas213@lehigh.edu>
2017-04-07 21:12:30 -0400

    added variable names to main

--- main.cpp
+++ main.cpp
@@ -5,6 +5,14 @@ double C = 1.;
 
 int main()
 {
-    Simulation S = Simulation("geo.txt", 2000, 1., 1., 0.6, 1.1, 1.);
+    unsigned STEPS = 2000;
+    double DX0 = 1.0;
+    double DT0 = 1.0;
+    double TAU = 0.6;
+    double RHOi= 1.1;
+    double RHOo= 1.0;
+
+    Simulation S = Simulation("geo.txt", STEPS, DX0, DT0, TAU, RHOi, RHOo);
+
     return 0;
 }
