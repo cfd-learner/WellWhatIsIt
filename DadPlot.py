f4c4ce0f9c9ef444f30cf45266123b9b1481df86
Greg Scott <gas213@lehigh.edu>
2017-07-02 21:47:37 -0400

    Added main to DadPlot

--- DadPlot.py
+++ DadPlot.py
@@ -4,38 +4,43 @@ from matplotlib.mlab import griddata
 import csv
 
 def scatter2D(filename):
-    
+
     x = []
     y = []
-    
+
     with open(filename) as csvfile:
         jawnreader = csv.reader(csvfile)
         for row in jawnreader:
             x.append(row[0])
             y.append(row[1])
-            
+
     plt.figure()
     plt.plot(x, y, ':')
     plt.show()
-    
+
 def contour2D(filename):
-    
+
     x = []
     y = []
     z = []
-    
+
     with open(filename) as csvfile:
         jawnreader = csv.reader(csvfile)
         for row in jawnreader:
             x.append(float(row[0]))
             y.append(float(row[1]))
             z.append(float(row[2]))
-    
+
     xi = np.linspace(min(x), max(x), 100)
     yi = np.linspace(min(y), max(y), 100)
     Z = griddata(x, y, z, xi, yi, 'linear')
     X, Y = np.meshgrid(xi, yi)
-            
+
     plt.figure()
     plt.contourf(X, Y, Z)
     plt.show()
+
+if __name__ == "__main__":
+    contour2D("output/rho/1000.csv")
+    contour2D("output/u/1000.csv")
+    contour2D("output/v/1000.csv")
